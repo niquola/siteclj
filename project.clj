74ec54076482806a412a8cfae7376e474210191c
niquola <niquola@gmail.com>
2015-03-07 01:48:25 +0300

    init

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject siteclj "0.1.0-SNAPSHOT"
+  :description "FIXME: write description"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [http-kit "2.1.16"]
+                 [hiccup "1.0.5"]
+                 [ring "1.3.2"]
+                 ])
